5a909d321fa28e0b2a7ed9754dfdd8ddaa8b7dfb
Kerem <238059619+kekerem14@users.noreply.github.com>
2025-10-21 21:01:39 +0300

    bolme.java

--- bolme.java
+++ bolme.java
@@ -1,14 +1,6 @@
 void main() {
-    Scanner input = new Scanner(System.in);
-
-    IO.print("Bölünen: ");
-    int sayi1 = input.nextInt();
-
-    IO.print("Bölen: ");
-    int sayi2 = input.nextInt();
-
+    int sayi1 = 10;
+    int sayi2 = 2;
     int sonuc = sayi1 / sayi2;
-    IO.println("Sonuç: " + sonuc);
-
-    input.close();
+    System.out.println("Sonuç:" + sonuc);
 }
