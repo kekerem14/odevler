dd46649c4cd549d028eda909c1d3d2cc4f016f4d
Kerem <238059619+kekerem14@users.noreply.github.com>
2025-10-21 20:57:14 +0300

    cikarma.java

--- cikarma.java
+++ cikarma.java
@@ -1,14 +1,6 @@
 void main() {
-    Scanner input = new Scanner(System.in);
-
-    IO.print("Birinci sayıyı girin: ");
-    int sayi1 = input.nextInt();
-
-    IO.print("İkinci sayıyı girin: ");
-    int sayi2 = input.nextInt();
-
+    int sayi1 = 10;
+    int sayi2 = 2;
     int sonuc = sayi1 - sayi2;
-    IO.println("Sonuç: " + sonuc);
-
-    input.close();
+    System.out.println("Sonuç:" + sonuc);
 }
