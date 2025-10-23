3ee2b41851ae94564feade2bfcfbe0158d5daa10
Kerem <238059619+kekerem14@users.noreply.github.com>
2025-10-23 22:55:43 +0300

    String metodları ödevi

---
+++ contains.java
@@ -0,0 +1,8 @@
+void main() {
+    String metin = "MerhAba";
+    if (metin.contains("a")) {
+        System.out.println("Metinde küçük a var.");
+    }else{
+        System.out.println("Metinde küçük a geçmiyor.");
+    }
+}
