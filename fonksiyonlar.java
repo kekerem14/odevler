fe7c7af49984a31e5b57aa2f5a2c8798f4f755c3
Kerem <238059619+kekerem14@users.noreply.github.com>
2025-11-14 19:37:34 +0300

    13-15 kasım ödevi fonksiyonlar

---
+++ fonksiyonlar.java
@@ -0,0 +1,19 @@
+String merhabaDe(){
+    return "Merhabalar! Nasılsınız?";
+}
+
+int carpma(int a,int b){
+    return a * b;
+}
+
+String vedalasma(){
+    return "Görüşürüz, iyi günler.";
+}
+
+
+
+void main() {
+    System.out.println(merhabaDe());
+    System.out.println("21 ve 3'ün çarpımı:"+carpma(21,3));
+    System.out.println(vedalasma());
+}
