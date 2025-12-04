1301e75f28ac71ca81e4e38e08d9bbc4bd6eb176
Kerem <238059619+kekerem14@users.noreply.github.com>
2025-12-04 21:34:16 +0300

    Update oopdersi.java

--- oopdersi.java
+++ oopdersi.java
@@ -3,12 +3,14 @@ class Ders {
     private String odev;
     private int yaziliNotu;
     private int sozluNotu;
+    private String olduguGunler;
 
-    public Ders(String ogretmen, String odev, int yaziliNotu, int sozluNotu){
+    public Ders(String ogretmen, String odev, int yaziliNotu, int sozluNotu, String olduguGunler){
         this.ogretmen = ogretmen;
         this.odev = odev;
         this.yaziliNotu = yaziliNotu;
         this.sozluNotu = sozluNotu;
+        this.olduguGunler = olduguGunler;
     }
 
     public void ekranaYazdir() {
@@ -16,12 +18,14 @@ class Ders {
         System.out.println("Ödev: " + odev);
         System.out.println("Yazılı Notu: " + yaziliNotu);
         System.out.println("Sözlü Notu: " + sozluNotu);
+        System.out.println("Dersin olduğu günler: " + olduguGunler);
     }
 }
 
 class Main {
     void main() {
-        Ders matematik = new Ders("Ahmet Hoca", "Yazı Ödevi", 95, 70);
+        Ders matematik;
+        matematik = new Ders("Ahmet Hoca", "Yazı Ödevi", 95, 70,"Perşembe, Cuma");
         matematik.ekranaYazdir();
     }
 }
