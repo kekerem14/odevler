4e5b02ecc493a23bc562bc6733487a10599dd472
Kerem <238059619+kekerem14@users.noreply.github.com>
2025-12-06 14:41:53 +0300

    Update ifelse.java
    Refactor main method to improve readability and structure.

--- ifelse.java
+++ ifelse.java
@@ -1,13 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
-        double sinav1 = 70; // 1. sınav puanı
-        double sinav2 = 80; // 2. sınav puanı
+void main() {
+    double sinav1 = 70;
+    double sinav2 = 80;
 
-        double ortalama = sinav1 * 0.4 + sinav2 * 0.6;
-
-        if (ortalama >= 75) System.out.println("Geçti");
-        else {
-            System.out.println("Kaldı");
-        }
+    double ortalama = sinav1 * 0.4 + sinav2 * 0.6;
+    if (ortalama >= 75) {
+        System.out.println("Geçti");
+    }else{
+        System.out.println("Kaldı");
     }
+
+
 }
