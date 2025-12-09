817e0162a723be0426840f3bc396da7fb74caabf
Kerem <238059619+kekerem14@users.noreply.github.com>
2025-12-09 20:16:07 +0300

    update oop

--- oopdersi.java
+++ oopdersi.java
@@ -3,14 +3,12 @@ class Ders {
     private String odev;
     private int yaziliNotu;
     private int sozluNotu;
-    private String olduguGunler;
 
-    public Ders(String ogretmen, String odev, int yaziliNotu, int sozluNotu, String olduguGunler){
+    public Ders(String ogretmen, String odev, int yaziliNotu, int sozluNotu){
         this.ogretmen = ogretmen;
         this.odev = odev;
         this.yaziliNotu = yaziliNotu;
         this.sozluNotu = sozluNotu;
-        this.olduguGunler = olduguGunler;
     }
 
     public void ekranaYazdir() {
@@ -18,14 +16,55 @@ class Ders {
         System.out.println("Ödev: " + odev);
         System.out.println("Yazılı Notu: " + yaziliNotu);
         System.out.println("Sözlü Notu: " + sozluNotu);
-        System.out.println("Dersin olduğu günler: " + olduguGunler);
+    }
+}
+
+class Pazartesi {
+    private Ders matematik;
+    private Ders edebiyat;
+
+    public Pazartesi() {
+        matematik = new Ders("Ahmet Hoca", "Yazı Ödevi", 95, 70);
+        edebiyat = new Ders("Mehmet Hoca", "Şiir Ezberleme", 85, 80);
+    }
+
+    public void ekranaYazdir() {
+        System.out.println("--- Pazartesi Dersleri ---");
+        System.out.println("Matematik:");
+        matematik.ekranaYazdir();
+        System.out.println();
+        System.out.println("Edebiyat:");
+        edebiyat.ekranaYazdir();
+        System.out.println();
+    }
+}
+
+class Sali {
+    private Ders matematik;
+    private Ders edebiyat;
+
+    public Sali() {
+        matematik = new Ders("Ahmet Hoca", "Problem Çözme", 88, 75);
+        edebiyat = new Ders("Mehmet Hoca", "Kompozisyon Yazma", 90, 85);
+    }
+
+    public void ekranaYazdir() {
+        System.out.println("--- Salı Dersleri ---");
+        System.out.println("Matematik:");
+        matematik.ekranaYazdir();
+        System.out.println();
+        System.out.println("Edebiyat:");
+        edebiyat.ekranaYazdir();
+        System.out.println();
     }
 }
 
 class Main {
     void main() {
-        Ders matematik;
-        matematik = new Ders("Ahmet Hoca", "Yazı Ödevi", 95, 70,"Perşembe, Cuma");
-        matematik.ekranaYazdir();
+        Pazartesi p = new Pazartesi();
+        Sali s = new Sali();
+
+        p.ekranaYazdir();
+        s.ekranaYazdir();
     }
 }
